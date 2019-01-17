d6cd2616209d71a69f04fd45b691b2c92404215d
yulei pang <yuleipang@yuleideMacBook-Pro.local>
2019-01-17 21:09:02 +0800

    Initial Commit

---
+++ PYLKVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PYLKVO
+//
+//  Created by yulei pang on 2019/1/17.
+//  Copyright © 2019 pangyulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PYLKVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PYLKVO
+//
+//  Created by yulei pang on 2019/1/17.
+//  Copyright © 2019 pangyulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
